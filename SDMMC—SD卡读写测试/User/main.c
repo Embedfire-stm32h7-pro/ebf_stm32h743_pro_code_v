927fbd0367eebbd53b5de908ddd94faae363dbad
hejiawen <hjw0415@utlook.com>
2019-10-12 11:30:41 +0800

    修改并添加说明文档

--- SDMMC—SD卡读写测试/User/main.c
+++ SDMMC—SD卡读写测试/User/main.c
@@ -4,11 +4,11 @@
   * @author  fire
   * @version V1.0
   * @date    2018-xx-xx
-  * @brief   FatFs移植测试
+  * @brief   SDMMC-SD卡读写测试
   ******************************************************************
   * @attention
   *
-  * 实验平台:野火 STM32H743开发板 
+  * 实验平台:野火 STM32H743 开发板 
   * 论坛    :http://www.firebbs.cn
   * 淘宝    :http://firestm32.taobao.com
   *
@@ -20,43 +20,8 @@
 #include "./usart/bsp_usart.h"
 #include "./sd_card/bsp_sdio_sd.h"
 #include "./key/bsp_key.h" 
-#include "./delay/core_delay.h"  
-/* FatFs includes component */
-#include "ff.h"
-#include "ff_gen_drv.h"
-#include "sd_diskio.h"
-/**
-  ******************************************************************************
-  *                              定义变量
-  ******************************************************************************
-  */
-char SDPath[4]; /* SD逻辑驱动器路径 */
-FATFS fs;													/* FatFs文件系统对象 */
-FIL fnew;													/* 文件对象 */
-FRESULT res_sd;                /* 文件操作结果 */
-UINT fnum;            			  /* 文件成功读写数量 */
-BYTE ReadBuffer[1024*4]={0};        /* 读缓冲区 */
-BYTE WriteBuffer[] =              /* 写缓冲区*/
-"欢迎使用野火STM32 H743开发板 今天是个好日子，新建文件系统测试文件\r\n";  
+#include "./delay/core_delay.h" 
 
-extern FATFS flash_fs;
-extern Diskio_drvTypeDef  SD_Driver;
-/**
-  * @brief  CPU L1-Cache enable.
-  * @param  None
-  * @retval None
-  */
-static void CPU_CACHE_Enable(void)
-{
-  /* Enable I-Cache */
-  SCB_EnableICache();
-
-  /* Enable D-Cache */
-  SCB_EnableDCache();
-
-  //将Cache设置write-through方式
-  SCB->CACR|=1<<2;
-}
 /**
   * @brief  主函数
   * @param  无
@@ -64,126 +29,33 @@ static void CPU_CACHE_Enable(void)
   */
 int main(void)
 {
-	int i;
     /* 系统时钟初始化成400MHz */
-    SystemClock_Config();
-  
-    CPU_CACHE_Enable();
-  
+    SystemClock_Config(); 
+    /* Enable I-Cache */
+    SCB_EnableICache(); 
+    /* Enable D-Cache */
+    SCB_EnableDCache();   
     LED_GPIO_Config();
     LED_BLUE;	
     /* 初始化USART1 配置模式为 115200 8-N-1 */
     UARTx_Config();	
     /* 初始化独立按键 */
-    printf("****** 这是一个SD卡文件系统实验 ******\r\n");
-    //链接驱动器，创建盘符
-    FATFS_LinkDriver(&SD_Driver, SDPath);
-    //在外部SD卡挂载文件系统，文件系统挂载时会对SD卡初始化
-    res_sd = f_mount(&fs,"0:",1);  
+    Key_GPIO_Config();
+    printf("\r\n欢迎使用野火  STM32 H743 开发板。\r\n");	
+    printf("在开始进行SD卡基本测试前，请给开发板插入32G以内的SD卡\r\n");			
+    printf("本程序会对SD卡进行非文件系统方式读写，会删除SD卡的文件系统\r\n");		
+    printf("实验后可通过电脑格式化或使用SD卡文件系统的例程恢复SD卡文件系统\r\n");		
+    printf("\r\n 但sd卡内的原文件不可恢复，实验前务必备份SD卡内的原文件！！！\r\n");		
+    printf("\r\n 若已确认，请按开发板的KEY1按键，开始SD卡测试实验....\r\n");	  
   
-    /*----------------------- 格式化测试 ---------------------------*/  
-    /* 如果没有文件系统就格式化创建创建文件系统 */
-    if(res_sd == FR_NO_FILESYSTEM)
-    {
-      printf("》SD卡还没有文件系统，即将进行格式化...\r\n");
-      /* 格式化 */
-      res_sd=f_mkfs("0:",0,0);							
-      
-      if(res_sd == FR_OK)
-      {
-        printf("》SD卡已成功格式化文件系统。\r\n");
-        /* 格式化后，先取消挂载 */
-        res_sd = f_mount(NULL,"0:",1);			
-        /* 重新挂载	*/			
-        res_sd = f_mount(&fs,"0:",1);
-      }
-      else
-      {
-        LED_RED;
-        printf("《《格式化失败。》》\r\n");
-        while(1);
-      }
-    }
-    else if(res_sd!=FR_OK)
-    {
-      printf("！！SD卡挂载文件系统失败。(%d)\r\n",res_sd);
-      printf("！！可能原因：SD卡初始化不成功。\r\n");
-      while(1);
-    }
-    else
-    {
-      printf("》文件系统挂载成功，可以进行读写测试\r\n");
-    }
-    
-    /*----------------------- 文件系统测试：写测试 -----------------------------*/
-    /* 打开文件，如果文件不存在则创建它 */
-    printf("\r\n****** 即将进行文件写入测试... ******\r\n");	
-    res_sd = f_open(&fnew, "0:FatFs读写测试文件.txt",FA_CREATE_ALWAYS | FA_WRITE );
-    if ( res_sd == FR_OK )
-    {
-      printf("》打开/创建FatFs读写测试文件.txt文件成功，向文件写入数据。\r\n");
-      /* 将指定存储区内容写入到文件内 */
-      res_sd=f_write(&fnew,WriteBuffer,sizeof(WriteBuffer),&fnum);
-      if(res_sd==FR_OK)
-      {
-        printf("》文件写入成功，写入字节数据：%d\n",fnum);
-        printf("》向文件写入的数据为：\r\n%s\r\n",WriteBuffer);
-      }
-      else
-      {
-        printf("！！文件写入失败：(%d)\n",res_sd);
-      }    
-      /* 不再读写，关闭文件 */
-      f_close(&fnew);
-    }
-    else
-    {	
-      LED_RED;
-      printf("！！打开/创建文件失败。\r\n");
-    }
-    
-  /*------------------- 文件系统测试：读测试 ------------------------------------*/
-    printf("****** 即将进行文件读取测试... ******\r\n");
-    res_sd = f_open(&fnew, "0:FatFs读写测试文件.txt", FA_OPEN_EXISTING | FA_READ); 
-    f_lseek(&fnew, 35555);
-    if(res_sd == FR_OK)
-    {
-      LED_GREEN;
-      printf("》打开文件成功。\r\n");
-      res_sd = f_read(&fnew, ReadBuffer, 800, &fnum); 
-      if(res_sd==FR_OK)
-      {
-//        printf("》文件读取成功,读到字节数据：%d\r\n",fnum);
-//        printf("》读取得的文件数据为：\r\n%s \r\n", ReadBuffer);	
-        int count = 0;
-        for(i = 0; i < fnum; i++)
-        {
-          count++;
-          printf("%x ", ReadBuffer[i]);
-          if(count%6 == 0)
-            printf("\n");
-        }
-      }
-      else
-      {
-        printf("！！文件读取失败：(%d)\n",res_sd);
-      }		
-    }
-    else
-    {
-      LED_RED;
-      printf("！！打开文件失败。\r\n");
-    }
-    /* 不再读写，关闭文件 */
-    f_close(&fnew);	
-    
-    /* 不再使用文件系统，取消挂载文件系统 */
-    f_mount(NULL,"0:",1);
-    
-    /* 操作完成，停机 */
     while(1)
     {
-    }
+        if(	Key_Scan(KEY1_GPIO_PORT,KEY1_PIN) == KEY_ON)
+        {
+            printf("\r\n开始进行SD卡读写实验\r\n");	
+            SD_Test();			            
+        }      
+    }			
 }
 
 /**
